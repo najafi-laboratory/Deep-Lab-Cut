c55cfb313cdd7e16b6b0425b55a5d74c43f5739a
Yuvraj <ydhadwal3@gatech.edu>
2024-07-24 13:39:24 -0400

    added way so I can process voltages and not wait on others

--- PACE_scripts/PaceBash.sh
+++ PACE_scripts/PaceBash.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH -JDLC-FN16			# Job Name
+#SBATCH -JDLC-FN14-20240710			# Job Name
 #SBATCH --account=gts-fnajafi3		# Charge Account
-#SBATCH -N1 --gres=gpu:V100:1     		# Number of nodes and cores per node required
+#SBATCH -N3 --gres=gpu:V100     		# Number of nodes and cores per node required
 #SBATCH --gres-flags=enforce-binding
 #SBATCH --mem-per-gpu=12G               # Memory per core
-#SBATCH -t2880                          # Duration of the job (Ex: 2880 mins or 2 days)
+#SBATCH -t1440                          # Duration of the job (Ex: 2880 mins or 2 days)
 #SBATCH -oReport-%j.out                         # Combined output and error messages file
 #SBATCH --mail-type=BEGIN,END,FAIL              # Mail preferences
 #SBATCH --mail-user=ydhadwal3@gatech.edu        # E-mail address for notifications
 
 source /storage/home/hcoda1/3/ydhadwal3/myenv/bin/activate
 
-cd /storage/coda1/p-fnajafi3/0/ydhadwal3/FN13_Joystick_0603-Yuvraj-2024-06-17             # Change to working directory
+cd /storage/coda1/p-fnajafi3/0/ydhadwal3/20240710-Yuvraj_Dhadwal-2024-07-24            # Change to working directory
 
 module load cuda/12.1.1-6oacj6
 
