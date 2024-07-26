307de1d887bd7339498b70d3dec50b9a3defbcb9
Yuvraj <ydhadwal3@gatech.edu>
2024-07-26 15:42:10 -0400

    labelling video

--- PACE_scripts/main.py
+++ PACE_scripts/main.py
@@ -2,14 +2,14 @@
 
 import deeplabcut
 
-yaml_path = "/storage/coda1/p-fnajafi3/0/ydhadwal3/FN14-20240523-Yuvraj-Dhadwal-2024-07-24/config.yaml"
-video_path = "/storage/coda1/p-fnajafi3/0/ydhadwal3/FN14-20240523-Yuvraj-Dhadwal-2024-07-24/videos"
-output_folder = "/storage/coda1/p-fnajafi3/0/ydhadwal3/FN14-20240523-Yuvraj-Dhadwal-2024-07-24/output"
+yaml_path = "/storage/coda1/p-fnajafi3/0/ydhadwal3/FN16-20240621-Yuvraj-Dhadwal-2024-07-24/config.yaml"
+video_path = "/storage/coda1/p-fnajafi3/0/ydhadwal3/FN16-20240621-Yuvraj-Dhadwal-2024-07-24/videos"
+output_folder = "/storage/coda1/p-fnajafi3/0/ydhadwal3/FN16-20240621-Yuvraj-Dhadwal-2024-07-24/output"
 
-deeplabcut.create_training_dataset(yaml_path, augmenter_type='imgaug')
+# deeplabcut.create_training_dataset(yaml_path, augmenter_type='imgaug')
 
-deeplabcut.train_network(yaml_path, maxiters=200000, saveiters=50000, max_snapshots_to_keep=3)
+# deeplabcut.train_network(yaml_path, maxiters=200000, saveiters=50000, max_snapshots_to_keep=3)
 
-#deeplabcut.evaluate_network(yaml_path, plotting=True)
+# deeplabcut.evaluate_network(yaml_path, plotting=True)
 
-# deeplabcut.analyze_videos(yaml_path, [video_path], videotype='avi', destfolder=output_folder, save_as_csv=True)
+deeplabcut.analyze_videos(yaml_path, [video_path], videotype='avi', destfolder=output_folder, save_as_csv=True)
