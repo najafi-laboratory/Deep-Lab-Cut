88b7907293ee792af16826e95ca045dde77c933a
Yuvraj <ydhadwal3@gatech.edu>
2024-09-06 13:46:41 -0400

    updated bash and main files based on new models

--- PACE_scripts/PaceBash.sh
+++ PACE_scripts/PaceBash.sh
@@ -4,12 +4,12 @@
 #SBATCH -N1 --gres=gpu:V100:1     		# Number of nodes and cores per node required
 #SBATCH --gres-flags=enforce-binding
 #SBATCH --mem-per-gpu=12G               # Memory per core
-#SBATCH -t1440                          # Duration of the job (Ex: 2880 mins or 2 days)
+#SBATCH -t30                          # Duration of the job (Ex: 2880 mins or 2 days)
 #SBATCH -oReport-%j.out                         # Combined output and error messages file
 #SBATCH --mail-type=BEGIN,END,FAIL              # Mail preferences
 #SBATCH --mail-user=ydhadwal3@gatech.edu        # E-mail address for notifications
 
-cd /storage/coda1/p-fnajafi3/0/ydhadwal3/20240710-Yuvraj_Dhadwal-2024-07-24     # Change to working directory
+cd /storage/coda1/p-fnajafi3/0/ydhadwal3/FN14-Passive-Yuvraj_Dhadwal-2024-07-24     # Change to working directory
 
 module load anaconda3
 conda activate DEEPLABCUT
