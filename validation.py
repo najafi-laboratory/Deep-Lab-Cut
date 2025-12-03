9320180b4e3d6bbc50cfa2e2bf934f7547466f89
Farzaneh Najafi <farznaj@gmail.com>
2025-12-03 16:04:38 -0500

    Fix CSV file path in validation.py

--- validation.py
+++ validation.py
@@ -98,9 +98,11 @@ if __name__ == "__main__":
     csv_dir_all = [d for d in os.listdir(path_to_model_output_folder) if d.endswith('.csv')]
 
     for csv_dir in csv_dir_all:
+
         # csv_dir = "/storage/home/hcoda1/8/fnajafi3/r-fnajafi3-0/DLC/Model/Body_tracking-Group Name-2025-11-05/output/yh24lg-trialvid-9-2025-10-23-120924-compressed_bGZTDDTRDLC_HrnetW48_Body_trackingNov5shuffle1_detector_best-100_snapshot_best-10.csv"	
+        csv_path = os.path.join(path_to_model_output_folder, csv_dir)
         
-        df = pd.read_csv(csv_dir)
+        df = pd.read_csv(csv_path)
         likelihoods = df.iloc[2:, list(range(3, len(df.columns), 3))]
 
         session_name = csv_dir.split("_HrnetW48")[0]
